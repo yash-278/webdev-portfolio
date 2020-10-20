51dca9454774e8e7357563004ef1636874cbc262
Yash Kadam <yashkadam278@gmail.com>
2020-10-20 09:20:10 +0530

    smooth-scroll bugs

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,15 +1,1 @@
-import jump from "node_modules/jump.js/dist/jump.module.js";
 
-var button1 = document.getElementById("btn1");
-var button2 = document.getElementById("btn2");
-var button3 = document.getElementById("btn3");
-
-button1.addEventListener("click", () => {
-  jump(".project");
-});
-button2.addEventListener("click", () => {
-  jump(".tech");
-});
-button3.addEventListener("click", () => {
-  jump(".contact");
-});
